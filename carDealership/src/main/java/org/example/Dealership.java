68d7ae403c5ce11539d4f3f10e8cc45d7d78c007
Preen Shrestha <Shresthapreen@gmail.com>
2023-11-01 15:16:11 -0400

    Updated/added more information

--- carDealership/src/main/java/org/example/Dealership.java
+++ carDealership/src/main/java/org/example/Dealership.java
@@ -10,8 +10,37 @@ public class Dealership {
     ArrayList<Vehicle> inventory;
 
     public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
 
+        ArrayList<Vehicle>inventory= new ArrayList<>();
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
 
     public List<Vehicle> getVehiclesByPrice(double min, double max) {
         return null;
--- carDealership/src/main/java/org/example/DealershipFileManager.java
+++ carDealership/src/main/java/org/example/DealershipFileManager.java
@@ -4,16 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class DealershipFileManager {
-
-    String filePath;
-
-    public DealershipFileManager(String filePath){
-        this.filePath = filePath;
-    }
-
-    public Dealership getDealership(){
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath)));
+public class DealershipFileManager{
+    public Dealership getDealership() {
+        try (BufferedReader reader = new BufferedReader(new FileReader()));
         String dealershipInfo = reader.readLine();
         String[] dealershipInfoArray = dealershipInfo.split("\\|");
         String name = dealershipInfoArray[0];
@@ -23,23 +16,21 @@ public class DealershipFileManager {
         Dealership dealership = new Dealership(name, address, phoneNumber);
 
         String line;
-        while ((line = reader.readLine()) != null){
+        while ((line = reader.readLine()) != null) {
             String[] vehicleInfoArray = line.split("\\|");
+
+            return null;
         }
-
-        return dealership;
-    } catch (IOException e){
-        return null;
     }
 
-    public void getDealership(){
-        return null;
-    }
-
-    public void saveDealership(){
-
-        return null;
+    public void saveDealership(String dealership) {
 
     }
-
 }
+
+
+
+
+
+
+
